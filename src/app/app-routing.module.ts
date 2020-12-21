8ef11437fbc741bf51fd0621435d71528d5cd261
Roderik Rasterhoff <s1125417@student.windesheim.nl>
2020-12-21 23:52:13 +0100

    Added route for weapon page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompareComponent } from './core/pages/compare/compare.component';
+import { WeaponPageComponent } from './core/pages/weapon-page/weapon-page.component';
 
 const routes: Routes = [
   {path: '', component: CompareComponent},
-  {path: '**', redirectTo: ''}
+  {path: '**', redirectTo: ''},
+  {path: 'weapon/:name', component: WeaponPageComponent}
 ];
 
 @NgModule({
